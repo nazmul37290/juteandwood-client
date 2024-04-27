bc1bfa5b9d2a1feab674311bed3dc507864d8238
nazmul37290 <nazmulnjm@gmail.com>
2024-04-27 18:35:22 +0600

    added enviormental variable for firebase key

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,16 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDYu0ePcBbcWEmmYRdfYqgyKgZNxT2XqLY",
-  authDomain: "juteandwood.firebaseapp.com",
-  projectId: "juteandwood",
-  storageBucket: "juteandwood.appspot.com",
-  messagingSenderId: "397843294388",
-  appId: "1:397843294388:web:ce8f5a5beb8d6c8b734fe1",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
