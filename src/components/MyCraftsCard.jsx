5fba5939bc7bc5367cefcaef6e3ab3f3fa9909d7
nazmul37290 <nazmulnjm@gmail.com>
2024-04-28 10:47:21 +0600

    added my arts and craft page

---
+++ src/components/MyCraftsCard.jsx
@@ -0,0 +1,68 @@
+import { Link } from "react-router-dom";
+import { FaEdit } from "react-icons/fa";
+import { MdDeleteForever } from "react-icons/md";
+const MyCraftsCard = ({ item }) => {
+  console.log(item);
+  return (
+    <div className="w-[300px] mx-auto md:w-[350px] h-[480px] rounded-2xl bg-[#efeae3]">
+      {/* upper side */}
+      <div className="bg-[#ded5c7] rounded-2xl w-full relative">
+        <div className=" mx-auto p-4 h-[220px] lg:h-[250px] w-[300px] lg:w-[350px] ">
+          <img
+            className="w-full rounded-2xl
+               h-full"
+            src={item.photo}
+            alt=""
+          />
+          <p className="bg-green-600 text-white font-bold px-2 py-1 absolute top-4 right-4 rounded-lg max-w-fit">
+            {item.stockStatus}
+          </p>
+        </div>
+      </div>
+      {/* lower side */}
+      <div className=" ">
+        <div className="px-4 py-4 space-y-1 text-lg">
+          <h3 className="text-[#3C2F19] uppercase min-h-16 overflow-hidden font-bold text-2xl">
+            {item.item_name}
+          </h3>
+          <div className="">
+            <p className="text-black">
+              <span className="font-semibold">Customization:</span>{" "}
+              {item.customization}
+            </p>
+            <div
+              className="flex
+               justify-between"
+            >
+              <p>
+                <span className="font-semibold">Ratings:</span> {item.rating}{" "}
+              </p>
+              <p className="font-bold text-lg">
+                {item.price} <span>tk</span>
+              </p>
+            </div>
+          </div>
+          <div className="flex justify-around">
+            <Link to={`/items/${item._id}`}>
+              <button className="btn mt-4 bg-green-600 text-white font-bold">
+                Update{" "}
+                <span className="text-base">
+                  <FaEdit></FaEdit>
+                </span>
+              </button>
+            </Link>
+
+            <button className="btn mt-4 bg-red-500 text-white font-bold">
+              Delete{" "}
+              <span className="text-xl">
+                <MdDeleteForever></MdDeleteForever>
+              </span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyCraftsCard;
--- src/pages/MyCrafts.jsx
+++ src/pages/MyCrafts.jsx
@@ -1,12 +1,29 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../provider/AuthProvider";
+
+import MyCraftsCard from "../components/MyCraftsCard";
 
 const MyCrafts = () => {
   const { user } = useContext(AuthContext);
+  const [myItems, setMyItems] = useState([]);
   console.log(user);
+  useEffect(() => {
+    fetch(`http://localhost:4000/items/email/${user.email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMyItems(data);
+      });
+  }, []);
+  console.log(myItems);
   return (
     <div className="max-w-6xl mx-auto mt-5">
       <h2 className="text-3xl font-bold text-center">My Arts & crafts</h2>
+      <div className="mt-5 grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {myItems.map((item) => {
+          return <MyCraftsCard key={item._id} item={item}></MyCraftsCard>;
+        })}
+      </div>
     </div>
   );
 };
